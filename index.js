9d3cbe4dcbcee9d458acf4774f6e7b5fe2c8b88b
Teja <starteja.007@gmail.com>
2022-09-11 20:31:44 +0530

    Add level 4 Authentication

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const md5=require("md5");
+const bcrypt=require("bcrypt");
 
+
+const saltRounds=12;
 
 var app = express();
 
@@ -41,34 +43,45 @@ app.get("/forgot",(req,res)=>{
 
 app.post("/signup",(req,res)=>{
 
-   const newUser=new User({
-      email:req.body.username,
-      password:md5(req.body.password)
-   });
-   newUser.save((err)=>{
-      if(err){
-         console.log(err);
-      }
-      else{
-         res.send("Successfull");
+   bcrypt.hash(req.body.password,saltRounds,(err,hash)=>{
+      const newUser=new User({
+         email:req.body.username,
+         password:hash
+      });
+      newUser.save((err)=>{
+         if(err){
+            console.log(err);
+         }
+         else{
+            res.send("Successfull");
+   
+         }
+   
+      });
+   })
 
-      }
-
-   });
+   
 });
 
 app.post("/login",(req,res)=>{
 
    const username=req.body.username;
-   const password=md5(req.body.password);
+   const password=req.body.password;
    console.log(username,password);
    User.findOne({email:username},(err,foundUser)=>{
       if(err){
          console.log(err);
       }
       else{
-         if(foundUser && foundUser.password===password){
-            res.send("loggedIn");
+         if(foundUser){
+            bcrypt.compare(password,foundUser.password,(err,result)=>{
+
+               if(result===true){
+                  res.send("loggedIn");
+               }
+
+            });
+            
          }
       }
    })
