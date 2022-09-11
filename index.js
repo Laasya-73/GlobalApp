83f2cbea53e05f461441c74b6fa7f77d55dde6df
Teja <starteja.007@gmail.com>
2022-09-11 22:46:24 +0530

    Added level 5 Authentication & Oauth2 not working

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const bcrypt=require("bcrypt");
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
+const GoogleStrategy=require("passport-google-oauth20").Strategy;
+const findOrCreate=require("mongoose-findorcreate");
 
 
-const saltRounds=12;
 
 var app = express();
+
 
 app.use(express.static("/public"));
 app.set("view engine", "ejs");
@@ -17,72 +21,129 @@ app.use(bodyParser.urlencoded({
    extended: true
  }));
 
- mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+app.use(session({
+   secret:"Our little secret.",
+   resave:false,
+   saveUninitialized:false,
+
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+
 
 const userSchema=new mongoose.Schema({
    email:String,
    password:String
 });
 
-
-
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User=new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+   clientID: process.env.CLIENT_ID,
+   clientSecret: process.env.CLIENT_SECRET,
+   callbackURL: "http://localhost:3000/auth/google/afterlogin",
+   userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+ },
+ function(accessToken, refreshToken, profile, cb) {
+   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+     return cb(err, user);
+   });
+ }
+));
+
 
 app.get("/",  (req, res) => {
    res.render("home");
 });
+
+app.get("/auth/google",(req,res)=>{
+
+   passport.authenticate("google");
+
+});
+
+app.get("/auth/google/afterlogin",
+passport.authenticate("google",{failureRedirect:"/login"}),(req,res)=>{
+   res.redirect("/afterlogin");
+
+});
+
 app.get("/login",(req,res)=>{
    res.render("login");
+});
+
+app.get("/register",(req,res)=>{
+   res.render("register");
 })
 app.get("/afterlogin",(req,res)=>{
-   res.render("afterlogin");
-})
+   if(req.isAuthenticated()){
+      res.render("afterlogin");
+   }
+   else{
+      res.redirect("/login");
+   }
+   
+});
 app.get("/forgot",(req,res)=>{
    res.render("forgot");
-})
+});
 
-app.post("/signup",(req,res)=>{
+app.get("/logout",(req,res)=>{
 
-   bcrypt.hash(req.body.password,saltRounds,(err,hash)=>{
-      const newUser=new User({
-         email:req.body.username,
-         password:hash
-      });
-      newUser.save((err)=>{
-         if(err){
-            console.log(err);
-         }
-         else{
-            res.send("Successfull");
-   
-         }
-   
-      });
-   })
+   req.logout((err)=>{
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  
+
+});
+
+
+app.post("/register",(req,res)=>{
+
+  User.register({username:req.body.username},req.body.password,(err,user)=>{
+      if(err){
+         console.log(err);
+         res.redirect("/login");
+      }
+      else{
+         passport.authenticate("local")(req,res,()=>{
+            res.redirect("/afterlogin");
+         })
+      }
+  })
 
    
 });
 
 app.post("/login",(req,res)=>{
 
-   const username=req.body.username;
-   const password=req.body.password;
-   console.log(username,password);
-   User.findOne({email:username},(err,foundUser)=>{
+   const user=new User({
+      username:req.body.username,
+      password:req.body.password
+   });
+
+   req.login(user,(err)=>{
       if(err){
          console.log(err);
       }
       else{
-         if(foundUser){
-            bcrypt.compare(password,foundUser.password,(err,result)=>{
-
-               if(result===true){
-                  res.send("loggedIn");
-               }
-
-            });
-            
-         }
+         passport.authenticate("local")(req,res,()=>{
+            res.redirect("/afterlogin");
+         });
       }
    })
 
