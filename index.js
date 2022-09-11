f33cbef24732f660e06ac2842bbd38786c0e252b
Teja <starteja.007@gmail.com>
2022-09-11 19:11:52 +0530

    Add environmental variables

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -20,9 +22,9 @@ const userSchema=new mongoose.Schema({
    password:String
 });
 
-const secret="Thisisthesecretpasswordfortheglobalapp";
 
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User=new mongoose.model("User",userSchema);
 
